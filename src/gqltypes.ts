15721b82435531bdc603650361593ed327f9e4f4
Simen Bekkhus <simenb@folio.no>
2022-02-01 14:45:48 +0100

    chore: update yarn and refresh lockfile

--- src/gqltypes.ts
+++ src/gqltypes.ts
@@ -1,5 +1,7 @@
 export type Maybe<T> = T | null;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -9,11 +11,9 @@ export type Scalars = {
   Float: number;
 };
 
-export type Query = {
-  __typename?: 'Query';
-  event?: Maybe<Event>;
-  receipt?: Maybe<Receipt>;
-  receipts: Array<Receipt>;
+export type Annotation = {
+  __typename?: 'Annotation';
+  id: Scalars['ID'];
 };
 
 export type Event = {
@@ -23,9 +23,11 @@ export type Event = {
   receipts: Array<Receipt>;
 };
 
-export type Annotation = {
-  __typename?: 'Annotation';
-  id: Scalars['ID'];
+export type Query = {
+  __typename?: 'Query';
+  event?: Maybe<Event>;
+  receipt?: Maybe<Receipt>;
+  receipts: Array<Receipt>;
 };
 
 export type Receipt = {
